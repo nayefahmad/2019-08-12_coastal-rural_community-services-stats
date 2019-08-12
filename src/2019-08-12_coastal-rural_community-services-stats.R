deb39eb2c46db2bfbc280518f04eb66d974550fd
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-12 14:47:41 -0700

    initial

---
+++ src/2019-08-12_coastal-rural_community-services-stats.R
@@ -0,0 +1,13 @@
+
+
+#'--- 
+#' title: "ALC rates for Coastal Rural Sites (SH, SGH, PRGH)"
+#' author: "Nayef Ahmad"
+#' date: "2019-08-12"
+#' output: 
+#'   html_document: 
+#'     keep_md: yes
+#'     code_folding: hide
+#'     toc: true
+#'     toc_float: true
+#' ---
