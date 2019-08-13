2c54efce50348192b59a7c473d04c724c9412abc
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-12 17:49:41 -0700

    denodo query for SMH

--- src/2019-08-12_coastal-rural_community-services-stats.R
+++ src/2019-08-12_coastal-rural_community-services-stats.R
@@ -11,3 +11,40 @@
 #'     toc: true
 #'     toc_float: true
 #' ---
+#' 
+
+
+# database cnx: --------
+library(odbc)
+library(dbplyr)
+library(tidyverse)
+library(denodoExtractor)
+
+setup_denodo()
+
+
+# 2) Validating with weekly iCare report ------------
+
+
+# sgh census patient-days and ALC patient days 
+df1.prgh_census <- 
+  vw_census %>% 
+  filter(facility_short_name == "SMH", 
+         census_date_id <= "20190801", 
+         census_date_id >= "20190726") %>% 
+  select(facility_short_name, 
+         census_date_id, 
+         patient_id, 
+         is_alc_p4p_definition_at_census, 
+         alc_category_desc) %>% 
+  collect %>% 
+  arrange(patient_id, 
+          census_date_id)
+
+# result: 
+df1.prgh_census
+summary(df1.prgh_census)
+
+# calc ALC rate: 
+sum(df1.prgh_census$is_alc)/nrow(df1.prgh_census)
+
