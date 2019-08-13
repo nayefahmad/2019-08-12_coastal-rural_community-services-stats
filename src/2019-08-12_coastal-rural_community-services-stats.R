1ccfe4990c407db3d8b4b8af8c1e1369965a6199
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-12 18:27:09 -0700

    validate using sql server

--- src/2019-08-12_coastal-rural_community-services-stats.R
+++ src/2019-08-12_coastal-rural_community-services-stats.R
@@ -48,3 +48,60 @@ summary(df1.prgh_census)
 # calc ALC rate: 
 sum(df1.prgh_census$is_alc)/nrow(df1.prgh_census)
 
+
+
+# 3) validate using sql server: -------
+
+#' See the iCare reports saved in the docs folder of this repo
+
+# connections: 
+cnx2 <- dbConnect(odbc::odbc(),
+                  dsn = "cnx_SPDBSCSTA001")
+census_sql_server <- dplyr::tbl(cnx2, 
+                     dbplyr::in_schema("ADTCMart.ADTC", 
+                                       "vwCensusFact"))
+
+# 3.1) PRGH ALC rate ------
+df1.prgch_sql_serv <- 
+  census_sql_server %>% 
+  filter(FacilityLongName == "Powell River General Hospital", 
+         CensusDate <= "2019-08-01", 
+         CensusDate >= "2019-07-26") %>% 
+  select(FacilityLongName, 
+         CensusDate, 
+         PatientID, 
+         PatientServiceDADDesc) %>% 
+  mutate(is_alc = ifelse(PatientServiceDADCode == "99", 1, 0)) %>% # show_query()
+  collect %>% 
+  arrange(PatientID, 
+          CensusDate) 
+
+sum(df1.prgch_sql_serv$is_alc)/nrow(df1.prgch_sql_serv)  # 0.2943262; matches with iCare report 
+
+
+# 3.2) SH ALC rate ------
+df2.sh_sql_serv <- 
+  census_sql_server %>% 
+  filter(FacilityLongName == "Sechelt Hospital", 
+         CensusDate <= "2019-07-18", 
+         CensusDate >= "2019-07-12", 
+         NursingUnitCode %in% c("1AA-SM", 
+                                "2A-SM", 
+                                "3A-SM", 
+                                "CCU-SM", 
+                                "DC-SM", 
+                                "EA-SM", 
+                                "MAT-SM", 
+                                "MH-SM", 
+                                "PAR-SM")) %>% 
+  select(FacilityLongName, 
+         CensusDate, 
+         PatientID, 
+         PatientServiceDADDesc) %>% 
+  mutate(is_alc = ifelse(PatientServiceDADCode == "99", 1, 0), 
+         is_valid = ifelse(PatientServiceDADDesc != "Invalid", 1, 0)) %>% # show_query()
+  collect %>% 
+  arrange(PatientID, 
+          CensusDate) 
+
+sum(df2.sh_sql_serv$is_alc)/sum(df2.sh_sql_serv$is_valid)
